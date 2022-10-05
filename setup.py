8f3bdc6994c2905a385aaebbac4ba04eb6c45b44
Nathan Hui <ntlhui@gmail.com>
2022-10-04 17:06:21 -0700

    Initial configuration

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='examplePackage',
+    name='FishSensePostCorrection',
     version='0.0.0.1',
     author='UCSD Engineers for Exploration',
     author_email='e4e@eng.ucsd.edu',
     entry_points={
         'console_scripts': [
-            'ExamplePythonConsoleScript = examplePackage.exampleModule:exampleEntryPoint'
+            'fishsense_postcorrection = e4e.postcorrection:main',
+            'fishsense_correctionAnalyze = e4e.analyze:main'
         ]
     },
     packages=find_packages(),
