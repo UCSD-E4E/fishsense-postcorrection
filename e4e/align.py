a5efd2c6e63dfadd717a8875e151d69a1f76e063
Nathan Hui <ntlhui@gmail.com>
2022-10-26 19:42:53 -0700

    Added speedups

--- e4e/align.py
+++ e4e/align.py
@@ -1,10 +1,13 @@
 from pathlib import Path
-import pyrealsense2 as rs
-import numpy as np
+from shutil import copy, move
+from typing import Dict
 import cv2 as cv
+import numpy as np
+import pyrealsense2 as rs
 from tqdm import tqdm
 
-def xy_align(bag_file: Path, output_dir: Path, n_metadata: int = 15):
+
+def xy_align(bag_file: Path, output_dir: Path, n_metadata: int = 5):
     pipeline = rs.pipeline()
     config = rs.config()
     
@@ -25,13 +28,15 @@ def xy_align(bag_file: Path, output_dir: Path, n_metadata: int = 15):
     align_to = rs.stream.color
     align = rs.align(align_to)
 
+    posPrev = 0
     
     try:
         with tqdm(total=duration) as pbar:
             while True:
-                posCurr = playback.get_position() / 1e9
-                pbar.update(posCurr)
                 frames = pipeline.wait_for_frames()
+                posCurr = playback.get_position() / 1e9
+                if posCurr < posPrev:
+                    break
                 aligned_frames = align.process(frames)
 
                 aligned_depth_frame = aligned_frames.get_depth_frame()
@@ -39,41 +44,92 @@ def xy_align(bag_file: Path, output_dir: Path, n_metadata: int = 15):
 
                 if aligned_depth_frame:
                     depth_image_counts = np.asanyarray(aligned_depth_frame.get_data())
-                    depth_timestamp = aligned_depth_frame.get_timestamp()
+                    depth_timestamp_s = aligned_depth_frame.get_timestamp() / 1e3
                     depth_frame_number = aligned_depth_frame.get_frame_number()
                     depth_image_m = (depth_image_counts * depth_scale).astype(np.float32)
-                    fname = output_dir.joinpath(f"{bag_file.stem}_Depth_{depth_timestamp:.9f}.tiff")
-                    cv.imwrite(fname.as_posix(), depth_image_m)
+                    stream_name = aligned_depth_frame.get_profile().stream_type().name
+                    fname = output_dir.joinpath(f"{bag_file.stem}_Depth_t{depth_timestamp_s:.9f}.tiff")
+                    mtd_fname = output_dir.joinpath(f'{bag_file.stem}_Depth_Metadata_t{depth_timestamp_s:.9f}.txt')
 
-                    with open(output_dir.joinpath(f'{bag_file.stem}_Depth_Metadata_{depth_timestamp:.9f}.txt'), 'w') as mtd_file:
-                        mtd_file.write(f'Stream: {aligned_depth_frame.get_profile().stream_type().name}\n')
+                    metadata = {
+                        "Stream": stream_name,
+                        'frame_number': depth_frame_number,
+                        'frame_timestamp': depth_timestamp_s
+                    }
 
-                        for i in range(n_metadata):
-                            mtd_val = rs.frame_metadata_value(i)
-                            if aligned_depth_frame.supports_frame_metadata(mtd_val):
-                                mtd_file.write(f'{mtd_val.name}: {aligned_depth_frame.get_frame_metadata(mtd_val)}\n')
-                        
-                        mtd_file.write(f'Frame Number: {depth_frame_number}\n')
+                    for i in range(n_metadata):
+                        mtd_val = rs.frame_metadata_value(i)
+                        if aligned_depth_frame.supports_frame_metadata(mtd_val):
+                            metadata[mtd_val.name] = aligned_depth_frame.get_frame_metadata(mtd_val)
+                    write_data(depth_image_m, fname, mtd_fname, metadata)
                 
                 if color_frame:
                     color_image = np.asanyarray(color_frame.get_data())
-                    color_timestamp = color_frame.get_timestamp()
+                    color_timestamp_s = color_frame.get_timestamp() / 1e3
                     color_frame_number = color_frame.get_frame_number()
-                    fname = output_dir.joinpath(f"{bag_file.stem}_Color_{color_timestamp:.9f}.png")
-                    cv.imwrite(fname.as_posix(), color_image)
-                    with open(output_dir.joinpath(f'{bag_file.stem}_Color_Metadata_{color_timestamp:.9f}.txt'), 'w') as mtd_file:
-                        mtd_file.write(f'Stream: {aligned_depth_frame.get_profile().stream_type().name}\n')
+                    stream_name = color_frame.get_profile().stream_type().name
+                    fname = output_dir.joinpath(f"{bag_file.stem}_Color_t{color_timestamp_s:.9f}.png")
+                    mtd_fname = output_dir.joinpath(f'{bag_file.stem}_Color_Metadata_t{color_timestamp_s:.9f}.txt')
 
-                        for i in range(n_metadata):
-                            mtd_val = rs.frame_metadata_value(i)
-                            if aligned_depth_frame.supports_frame_metadata(mtd_val):
-                                mtd_file.write(f'{mtd_val.name}: {aligned_depth_frame.get_frame_metadata(mtd_val)}\n')
+                    metadata = {
+                        "Stream": stream_name,
+                        'frame_number': color_frame_number,
+                        'frame_timestamp': color_timestamp_s
+                    }
 
-                        mtd_file.write(f'Frame Number: {color_frame_number}\n')
+                    for i in range(n_metadata):
+                        mtd_val = rs.frame_metadata_value(i)
+                        if color_frame.supports_frame_metadata(mtd_val):
+                            metadata[mtd_val.name] = color_frame.get_frame_metadata(mtd_val)
+                    write_data(color_image, fname, mtd_fname, metadata)
                 
-                posNext = playback.get_position() / 1e9
-                if posNext < posCurr:
-                    break
+                pbar.update(posCurr - posPrev)
+                posPrev = posCurr
 
     finally:
         pipeline.stop()
+
+def write_data(depth_image_m, img_fname, mtd_fname, metadata):
+    cv.imwrite(img_fname.as_posix(), depth_image_m)
+
+    with open(mtd_fname, 'w') as mtd_file:
+        for k, v in metadata.items():
+            mtd_file.write(f'{k}: {v}\n')
+
+    
+def t_align(input_dir: Path, output_dir: Path, label_dir: Path, max_permissible_difference_s: float = 0.1):
+    color_frame_t: Dict[float, Path] = {}
+    for color_frame in input_dir.glob('*_Color_t[0-9.]*'):
+        fname = color_frame.stem
+        t = float(fname[fname.find('_t') + 2:])
+        color_frame_t[t] = color_frame
+    
+    color_times = np.array(list(color_frame_t.keys()))
+
+    depth_frame_t: Dict[float, Path] = {}
+    for depth_frame in input_dir.glob('*_Depth_t[0-9.]*'):
+        fname = depth_frame.stem
+        t = float(fname[fname.find('_t') + 2:])
+        depth_frame_t[t] = depth_frame
+
+    for idx, depth_time in enumerate(depth_frame_t):
+        deltas = np.abs(color_times - depth_time)
+        min_time_idx = np.argmin(deltas)
+        if deltas[min_time_idx] >= max_permissible_difference_s:
+            print("Match not found")
+            continue
+        color_time = color_times[min_time_idx]
+
+
+        depth_files = input_dir.glob(f"*Depth*_t{depth_time:.9f}*")
+        color_files = input_dir.glob(f"*Color*_t{color_time:.9f}*")
+
+        frame_folder = output_dir.joinpath(f'frame_{idx:06d}')
+        frame_folder.mkdir(exist_ok=True, parents=True)
+
+        for depth_file in depth_files:
+            move(depth_file, frame_folder.joinpath(depth_file.name))
+        for color_file in color_files:
+            if color_file.suffix.endswith('png'):
+                copy(color_file, label_dir.joinpath(color_file.name))
+            move(color_file, frame_folder.joinpath(color_file.name))
--- e4e/extract.py
+++ e4e/extract.py
@@ -3,17 +3,19 @@ from pathlib import Path
 from platform import system
 import subprocess
 
-from e4e.align import xy_align
+from e4e.align import t_align, xy_align
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('input')
     parser.add_argument('output_directory')
+    parser.add_argument('label_directory')
     args = parser.parse_args()
 
     input_path = Path(args.input)
     output_dir = Path(args.output_directory)
+    label_dir = Path(args.label_directory)
 
     if not input_path.is_file():
         raise RuntimeError("Not a file")
@@ -22,12 +24,16 @@ def main():
         output_dir.mkdir(parents=True)
     if not output_dir.is_dir():
         raise RuntimeError("Not a directory")
-    
-    output_path = output_dir.joinpath(input_path.stem)
 
-    # extract_rgb(input_path, output_path)
-    # extract_depth(input_path, output_path)
+    if not label_dir.exists():
+        label_dir.mkdir(parents=True)
+    if not label_dir.is_dir():
+        raise RuntimeError("Not a directory")
+    if any(label_dir.iterdir()):
+        raise RuntimeError("Not empty directory!")
+    
     xy_align(input_path, output_dir)
+    t_align(output_dir=output_dir, input_dir=output_dir, label_dir=label_dir)
 
 def extract_depth(input_path: Path, output_path: Path):
     if system() == 'Windows':
