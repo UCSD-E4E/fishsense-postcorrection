a232ea6aaf83236b322fc344f8825c53ec4c5607
Nathan Hui <ntlhui@gmail.com>
2022-11-04 12:45:30 -0700

    Added pipelining

--- runner.py
+++ runner.py
@@ -1,8 +1,13 @@
 # %%
 from pathlib import Path
-from e4e.align import xy_auto_align, t_align
-from tqdm import tqdm
+from queue import Queue
+from shutil import copy
+from threading import Thread
+
 import yaml
+from tqdm import tqdm
+
+from e4e.align import t_align, xy_auto_align
 
 # %%
 deployment_root_path = Path('/home/ntlhui/google_drive/Test Data/2022-05 Reef Deployment/usa_florida')
@@ -10,15 +15,17 @@ target_path = Path('/home/ntlhui/fishsense/nas/data/2022-05 Reef Deployment outp
 progress_path = Path('/home/ntlhui/fishsense/progress.yaml')
 
 # %%
-output_dirs = ['_'.join(bag_file.relative_to(deployment_root_path).with_suffix('').parts) for bag_file in deployment_root_path.glob('**/*.bag')]
-label_dirs = ['_'.join(bag_file.relative_to(deployment_root_path).with_suffix('').parts) + '_label' for bag_file in deployment_root_path.glob('**/*.bag')]
+bag_files = list(deployment_root_path.glob('**/*.bag'))
+output_dirs = ['_'.join(bag_file.relative_to(deployment_root_path).with_suffix('').parts) for bag_file in bag_files]
+label_dirs = ['_'.join(bag_file.relative_to(deployment_root_path).with_suffix('').parts) + '_label' for bag_file in bag_files]
+tmp_paths = [Path('/tmp/fishsense').joinpath('_'.join(bag_file.relative_to(deployment_root_path).parts)) for bag_file in bag_files]
 
 # %%
 for dir in tqdm(output_dirs):
     target_path.joinpath(dir).mkdir(parents=True, exist_ok=True)
 for dir in tqdm(label_dirs):
     target_path.joinpath(dir).mkdir(parents=True, exist_ok=True)
-
+Path('/tmp/fishsense').mkdir(parents=True, exist_ok=True)
 # %%
 progress_path.touch(exist_ok=True)
 file_progress = {}
@@ -26,28 +33,46 @@ with open(progress_path, 'r') as f:
     recorded = yaml.safe_load(f)
     if recorded:
         file_progress.update(recorded)
-for idx, bag_file in tqdm(enumerate(deployment_root_path.glob('**/*.bag'))):
-    if bag_file.as_posix() in file_progress:
-        continue
-    output_dir = target_path.joinpath(output_dirs[idx])
-    label_dir = target_path.joinpath(label_dirs[idx])
-    print(bag_file.as_posix())
-    try:
-        xy_auto_align(bag_file=bag_file, output_dir=output_dir)
-        t_align(output_dir=output_dir, input_dir=output_dir, label_dir=label_dir)
-    except Exception as e:
+
+bag_files = [f for f in bag_files if f.as_posix() not in file_progress]
+
+tmp_path_queue = Queue()
+def copy_thread_fn(bag_files, tmp_paths, tmp_path_queue: Queue):
+    assert(len(bag_files) == len(tmp_paths))
+    for idx in range(len(bag_files)):
+        bag_file = bag_files[idx]
+        tmp_path = tmp_paths[idx]
+        copy(bag_file, tmp_path)
+        tmp_path_queue.put(tmp_path)
+        print(f"Copied {bag_file} to {tmp_path}")
+
+def process_thread_fn(tmp_path_queue: Queue, output_dirs, label_dirs):
+    for idx in len(output_dirs):
+        bag_file = tmp_path_queue.get()
+        output_dir = target_path.joinpath(output_dirs[idx])
+        label_dir = target_path.joinpath(label_dirs[idx])
+        print(bag_file.as_posix())
+        try:
+            xy_auto_align(bag_file=bag_file, output_dir=output_dir)
+            t_align(output_dir=output_dir, input_dir=output_dir, label_dir=label_dir)
+        except Exception as e:
+            file_progress[bag_file.as_posix()] = {
+                'status': False,
+                'error': str(e)
+            }
+            with open(progress_path, 'w') as f:
+                yaml.safe_dump(file_progress, f)
+            continue
+        print("done")
         file_progress[bag_file.as_posix()] = {
-            'status': False,
-            'error': str(e)
+            'status': True
         }
         with open(progress_path, 'w') as f:
             yaml.safe_dump(file_progress, f)
-        continue
-    print("done")
-    file_progress[bag_file.as_posix()] = {
-        'status': True
-    }
-    with open(progress_path, 'w') as f:
-        yaml.safe_dump(file_progress, f)
 
-
+copy_thread = Thread(target=copy_thread_fn, args=(bag_files, tmp_paths, tmp_path_queue))
+copy_thread.start()
+process_thread = Thread(target=process_thread_fn, args=(tmp_path_queue, output_dirs, label_dirs))
+process_thread.start()
+process_thread.join()
+copy_thread.join()
