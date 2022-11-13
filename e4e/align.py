f9648a9b4d49e2953c00de7d3b3c37938098e95e
Nathan Hui <ntlhui@gmail.com>
2022-11-12 16:05:23 -0800

    Fixing some linting

--- e4e/align.py
+++ e4e/align.py
@@ -13,12 +13,12 @@ from e4e.timeranges import in_timeranges
 def xy_auto_align(bag_file: Path, output_dir: Path, n_metadata: int = 5):
     pipeline = rs.pipeline()
     config = rs.config()
-    
+
     rs.config.enable_device_from_file(config, bag_file.as_posix())
     config.enable_all_streams()
 
     profile = pipeline.start(config)
-    
+
     device = profile.get_device()
     playback = device.as_playback()
     playback.set_real_time(False)
@@ -32,7 +32,7 @@ def xy_auto_align(bag_file: Path, output_dir: Path, n_metadata: int = 5):
     align = rs.align(align_to)
 
     posPrev = 0
-    
+
     try:
         with tqdm(total=duration) as pbar:
             while True:
@@ -52,8 +52,10 @@ def xy_auto_align(bag_file: Path, output_dir: Path, n_metadata: int = 5):
                     depth_frame_number = aligned_depth_frame.get_frame_number()
                     depth_image_m = (depth_image_counts * depth_scale).astype(np.float32)
                     stream_name = aligned_depth_frame.get_profile().stream_type().name
-                    fname = output_dir.joinpath(f"{bag_file.stem}_Depth_t{depth_timestamp_s:.9f}.tiff")
-                    mtd_fname = output_dir.joinpath(f'{bag_file.stem}_Depth_Metadata_t{depth_timestamp_s:.9f}.txt')
+                    depth_img_path = f"{bag_file.stem}_Depth_t{depth_timestamp_s:.9f}.tiff"
+                    fname = output_dir.joinpath(depth_img_path)
+                    depth_metadata_path = f'{bag_file.stem}_Depth_Metadata_t{depth_timestamp_s:.9f}.txt'
+                    mtd_fname = output_dir.joinpath(depth_metadata_path)
 
                     metadata = {
                         "Stream": stream_name,
@@ -66,7 +68,7 @@ def xy_auto_align(bag_file: Path, output_dir: Path, n_metadata: int = 5):
                         if aligned_depth_frame.supports_frame_metadata(mtd_val):
                             metadata[mtd_val.name] = aligned_depth_frame.get_frame_metadata(mtd_val)
                     write_data(depth_image_m, fname, mtd_fname, metadata)
-                
+
                 if color_frame:
                     color_image = np.asanyarray(color_frame.get_data())
                     color_timestamp_s = color_frame.get_timestamp() / 1e3
@@ -86,7 +88,7 @@ def xy_auto_align(bag_file: Path, output_dir: Path, n_metadata: int = 5):
                         if color_frame.supports_frame_metadata(mtd_val):
                             metadata[mtd_val.name] = color_frame.get_frame_metadata(mtd_val)
                     write_data(color_image, fname, mtd_fname, metadata)
-                
+
                 pbar.update(posCurr - posPrev)
                 posPrev = posCurr
 
@@ -100,7 +102,7 @@ def write_data(depth_image_m, img_fname, mtd_fname, metadata):
         for k, v in metadata.items():
             mtd_file.write(f'{k}: {v}\n')
 
-    
+
 def t_align(input_dir: Path, output_dir: Path, label_dir: Path, max_permissible_difference_s: float = 0.1):
     bag_file_name = ''
     color_frame_t: Dict[float, Path] = {}
@@ -109,7 +111,7 @@ def t_align(input_dir: Path, output_dir: Path, label_dir: Path, max_permissible_
         t = float(fname[fname.find('_t') + 2:])
         color_frame_t[t] = color_frame
         bag_file_name = color_frame.name[:color_frame.name.find('_Color_t')]
-    
+
     color_times = list(color_frame_t.keys())
 
     depth_frame_t: Dict[float, Path] = {}
