bd21033c6619700a58eb8bab5b635b36995e9296
Nathan Hui <ntlhui@gmail.com>
2022-11-21 17:13:42 -0800

    Fixed detect call

--- e4e/fishfinder.py
+++ e4e/fishfinder.py
@@ -1,32 +1,45 @@
+"""Fish Finding utility
+"""
+from argparse import ArgumentParser
 from pathlib import Path
 from typing import List
-from argparse import ArgumentParser
+
 from e4e.detection_code.detect_function import detect
 
+
 def find_fish(folder: Path) -> List[Path]:
+    """Returns a list of images that most likely have fish in them
+
+    Args:
+        folder (Path): Directory to find fish in
+
+    Returns:
+        List[Path]: List of png files most likely containing fish
+    """
     all_images = folder.glob('*.png')
-    list =detect(all_images,0.3,0.45)
-    file = open(FLAGS.output + image_name + ".txt", "+w")
-    for i in list:
-        file.write(i)
-        file.write("\n")
-    raise NotImplementedError
+    return detect(
+        images=all_images,
+        iou=0.3,
+        score=0.45)
 
 def fishfinder_main():
+    """Top Level function for fishfinder
+
+    Raises:
+        RuntimeError: Input Path is not a directory/does not exist
+    """
     parser = ArgumentParser()
     parser.add_argument('input_path')
     parser.add_argument('output_file')
-    
+
     args = parser.parse_args()
     input_path = Path(args.input_path)
     output_file = Path(args.output_file)
-    if not input_path.exists():
-        raise RuntimeError()
     if not input_path.is_dir():
         raise RuntimeError()
-    
+
     fish_images = find_fish(folder=input_path)
-    with open(output_file, 'w') as f:
+    with open(output_file, 'w', encoding='ascii') as f:
         for img in fish_images:
             f.write(f'{img.relative_to(input_path).as_posix()}\n')
 
