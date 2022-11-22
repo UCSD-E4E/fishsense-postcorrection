768bde1ffb2dd7888f6e8bd13deeca62a0050d6c
Nathan Hui <ntlhui@gmail.com>
2022-11-21 17:15:37 -0800

    Updated imports and weights location

--- e4e/detection_code/detect_function.py
+++ e4e/detection_code/detect_function.py
@@ -1,11 +1,10 @@
-import math
 import os
-import sys
+from pathlib import Path
+from typing import List
 
 import cv2
 import numpy as np
 import tensorflow as tf
-from absl import app, flags, logging
 from absl.flags import FLAGS
 from PIL import Image
 from tensorflow.compat.v1 import ConfigProto, InteractiveSession
@@ -20,84 +19,81 @@ physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-def detect(iou,score,images):
-    list_fishes = []
+def detect(iou: float, score: float, images: List[Path]) -> List[Path]:
+    list_fishes: List[Path] = []
     config = ConfigProto()
     config.gpu_options.allow_growth = True
-    saved_model_loaded = tf.saved_model.load("./checkpoints/yolov4-416", tags=[tag_constants.SERVING])
+    saved_model_loaded = tf.saved_model.load("./yolov4-416", tags=[tag_constants.SERVING])
     for file in images:
-            original_image = cv2.imread(file)
-            original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+        original_image = cv2.imread(file.as_posix())
+        original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 
-            image_data = cv2.resize(original_image, (416, 416))
-            image_data = image_data / 255.
+        image_data = cv2.resize(original_image, (416, 416))
+        image_data = image_data / 255.
 
-            # get image name by using split method
-            image_name = file.split('/')[-1]
-            image_name = file.split('.')[0]
 
-            images_data = []
-            for i in range(1):
-                images_data.append(image_data)
-            images_data = np.asarray(images_data).astype(np.float32)
-            infer = saved_model_loaded.signatures['serving_default']
-            batch_data = tf.constant(images_data)
-            pred_bbox = infer(batch_data)
-            for key, value in pred_bbox.items():
-                boxes = value[:, :, 0:4]
-                pred_conf = value[:, :, 4:]
+        images_data = []
+        for i in range(1):
+            images_data.append(image_data)
+        images_data = np.asarray(images_data).astype(np.float32)
+        infer = saved_model_loaded.signatures['serving_default']
+        batch_data = tf.constant(images_data)
+        pred_bbox = infer(batch_data)
+        for key, value in pred_bbox.items():
+            boxes = value[:, :, 0:4]
+            pred_conf = value[:, :, 4:]
 
-            # run non max suppression on detections
-            boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
-                boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
-                scores=tf.reshape(
-                    pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
-                max_output_size_per_class=50,
-                max_total_size=50,
-                iou_threshold=iou,
-                score_threshold=score
-            )
+        # run non max suppression on detections
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=iou,
+            score_threshold=score
+        )
 
-            # format bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, xmax, ymax
-            original_h, original_w, _ = original_image.shape
-            bboxes = utils.format_boxes(boxes.numpy()[0], original_h, original_w)
+        # format bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, xmax, ymax
+        original_h, original_w, _ = original_image.shape
+        bboxes = utils.format_boxes(boxes.numpy()[0], original_h, original_w)
 
-            ##############################################################################################
+        ##############################################################################################
 
-            ##############################################################################################
+        ##############################################################################################
 
-            # hold all detection data in one variable
-            pred_bbox = [bboxes, scores.numpy()[0], classes.numpy()[0], valid_detections.numpy()[0]]
+        # hold all detection data in one variable
+        pred_bbox = [bboxes, scores.numpy()[0], classes.numpy()[0], valid_detections.numpy()[0]]
 
-            # read in all class names from config
-            class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+        # read in all class names from config
+        class_names = utils.read_class_names(cfg.YOLO.CLASSES)
 
-            # by default allow all classes in .names file
-            allowed_classes = list(class_names.values())
+        # by default allow all classes in .names file
+        allowed_classes = list(class_names.values())
 
-            # custom allowed classes (uncomment line below to allow detections for only people)
-            # allowed_classes = ['person']
+        # custom allowed classes (uncomment line below to allow detections for only people)
+        # allowed_classes = ['person']
 
-            counted_things = (count_objects(pred_bbox, by_class=True, allowed_classes=['Fish']))
-            classesfound = []
-            for i in range(len(class_names)):
-                classesfound.append(class_names[i])
-            fishes = []
-            if ('Fish' in counted_things):
-                fishes_found = counted_things['Fish']
+        counted_things = (count_objects(pred_bbox, by_class=True, allowed_classes=['Fish']))
+        classesfound = []
+        for i in range(len(class_names)):
+            classesfound.append(class_names[i])
+        fishes = []
+        if ('Fish' in counted_things):
+            fishes_found = counted_things['Fish']
 
-                for i in range(fishes_found):
-                    fishes.append([bboxes[i][0], bboxes[i][1], bboxes[i][2], bboxes[i][3]])
+            for i in range(fishes_found):
+                fishes.append([bboxes[i][0], bboxes[i][1], bboxes[i][2], bboxes[i][3]])
 
-            # if crop flag is enabled, crop each detection and save it as new image
+        # if crop flag is enabled, crop each detection and save it as new image
 
-            # if count flag is enabled, perform counting of objects
-            image = utils.draw_bbox(original_image, pred_bbox, False, allowed_classes=allowed_classes,
-                                        show_label=True, read_plate=False)
+        # if count flag is enabled, perform counting of objects
+        image = utils.draw_bbox(original_image, pred_bbox, False, allowed_classes=allowed_classes,
+                                    show_label=True, read_plate=False)
 
-            image = Image.fromarray(image.astype(np.uint8))
+        image = Image.fromarray(image.astype(np.uint8))
 
-            image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
-            list_fishes.append(image_name)
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+        list_fishes.append(file)
 
     return list_fishes
