bb45fc6bc7028586e25be3edafb78b0a288427eb
Nathan Hui <ntlhui@gmail.com>
2022-11-23 14:19:56 -0800

    Added job refactor

--- runner.py
+++ runner.py
@@ -1,29 +1,87 @@
 """Extracts all data for labeling
 """
-from argparse import ArgumentParser
-from dataclasses import dataclass
 import os
+from argparse import ArgumentParser
 from pathlib import Path
 from queue import Queue
 from shutil import copy
 from threading import Thread
-from typing import List, Dict, Tuple
+from typing import Dict, List, Tuple
 
 import yaml
-from tqdm import tqdm
 
 from e4e.align import t_align, xy_auto_align
 
-@dataclass
+
 class Job:
     """Job Class - contains job definition and progress tracking
     """
-    bag_file: Path
-    tmp_path: Path
-    output_dir: Path
-    label_dir: Path
 
-def copy_thread_fn(bag_files: List[Path], tmp_paths: List[Path], tmp_path_queue: "Queue[Path]"):
+    def __init__(self,
+            bag_file: Path,
+            data_root: Path,
+            target_root: Path,
+            cache_path: Path) -> None:
+        self.__bag_file = bag_file.resolve()
+        self.__data_root = data_root.resolve()
+        self.__target_root = target_root.resolve()
+        self.__cache_path = cache_path.resolve()
+
+    @property
+    def bag_path(self) -> Path:
+        """Absolute bag file path
+
+        Returns:
+            Path: Absolute bag file path
+        """
+        return self.__bag_file
+
+    @property
+    def bag_file(self) -> Path:
+        """Bag File Path relative to the deployment root
+
+        Returns:
+            Path: Relative path of bag file
+        """
+        return self.__bag_file.relative_to(self.__data_root)
+
+    @property
+    def output_folder(self) -> Path:
+        """Directory in which paired RGB and depth frames will go
+
+        Returns:
+            Path: Paired frame folder
+        """
+        return self.__target_root.joinpath('_'.join(self.bag_file.with_suffix('').parts))
+
+    @property
+    def label_dir(self) -> Path:
+        """Directory in which RGB stills for labeling will go
+
+        Returns:
+            Path: Label folder
+        """
+        return self.__target_root.joinpath('_'.join(self.bag_file.with_suffix('').parts) + '_label')
+
+    @property
+    def tmp_path(self) -> Path:
+        """Cached path of bag file
+
+        Returns:
+            Path: cached bag file path
+        """
+        return self.__cache_path.joinpath(self.reference_name)
+
+    @property
+    def reference_name(self) -> str:
+        """Returns the single part reference name for the job
+
+        Returns:
+            str: Reference string
+        """
+        return '_'.join(self.bag_file.parts)
+
+def copy_thread_fn(jobs: List[Job], job_queue: "Queue[Job]"):
     """Copy Thread
 
     Args:
@@ -31,25 +89,20 @@ def copy_thread_fn(bag_files: List[Path], tmp_paths: List[Path], tmp_path_queue:
         tmp_paths (List[Path]): List of temporary paths to place the ROSBAGS
         tmp_path_queue (Queue[Path]): Queue to enqueue ready ROSBAGS
     """
-    assert len(bag_files) == len(tmp_paths)
-    for idx in range(len(bag_files)): # pylint: disable=consider-using-enumerate
-        bag_file = bag_files[idx]
-        tmp_path = tmp_paths[idx]
-        if tmp_path.exists():
-            if tmp_path.stat().st_size != bag_file.stat().st_size:
-                copy(bag_file, tmp_path)
+    for job in jobs:
+        if job.tmp_path.exists():
+            if job.tmp_path.stat().st_size != job.bag_path.stat().st_size:
+                copy(job.bag_path, job.tmp_path)
         else:
-            copy(bag_file, tmp_path)
-        tmp_path_queue.put(tmp_path)
-        print(f"Copied {bag_file} to {tmp_path}")
+            copy(job.bag_path, job.tmp_path)
+        job_queue.put(job)
+        print(f'Copied {job.bag_path} to {job.tmp_path}')
 
 def process_thread_fn(
-        tmp_path_queue: "Queue[Path]",
-        output_dirs: List[str],
-        label_dirs: List[str],
-        target_path: Path,
+        job_queue: "Queue[Job]",
         progress_path: Path,
         file_progress: Dict[str, Dict],
+        num_jobs: int,
         bypass_xy_align_errors: bool = False):
     """Processing thread function
 
@@ -62,33 +115,33 @@ def process_thread_fn(
         file_progress (Dict[str, Dict]): File progress object
         bypass_xy_align_errors (bool): xy_align error bypass
     """
-    for idx, _ in enumerate(output_dirs):
-        bag_file = tmp_path_queue.get()
-        output_dir = target_path.joinpath(output_dirs[idx])
-        label_dir = target_path.joinpath(label_dirs[idx])
-        print(bag_file.as_posix())
+    for _ in enumerate(range(num_jobs)):
+        job = job_queue.get()
+        print(job.bag_file.as_posix())
+
         try:
             xy_auto_align(
-                bag_file=bag_file,
-                output_dir=output_dir,
-                ignore_errors=bypass_xy_align_errors)
-            t_align(output_dir=output_dir, input_dir=output_dir, label_dir=label_dir)
-        except Exception as exp: # pylint: disable=broad-except
-            file_progress[bag_file.name] = {
-                'status': False,
-                'error': str(exp)
+                bag_file=job.tmp_path,
+                output_dir=job.output_folder,
+                ignore_errors=bypass_xy_align_errors
+            )
+            t_align(
+                output_dir=job.output_folder,
+                input_dir=job.output_folder,
+                label_dir=job.label_dir
+            )
+            file_progress[job.reference_name] = {
+                'status': True
             }
+        except Exception as exc: # pylint: disable=broad-except
+            file_progress[job.reference_name] = {
+                'status': False,
+                'error': str(exc)
+            }
+        finally:
             with open(progress_path, 'w', encoding='utf-8') as handle:
                 yaml.safe_dump(file_progress, handle)
-            os.remove(bag_file)
-            continue
-        print("done")
-        file_progress[bag_file.name] = {
-            'status': True
-        }
-        with open(progress_path, 'w', encoding='utf-8') as handle:
-            yaml.safe_dump(file_progress, handle)
-        os.remove(bag_file)
+            os.remove(job.tmp_path)
 
 def t_align_pipelined(input_queue: "Queue[Tuple[Path, Path]]", label_dirs: List[str]):
     """Pipelined version of temporal alignment
@@ -123,10 +176,9 @@ def run():
     # bypass_xy_align_errors = True
 
     deployment_root_path = Path(args.input_path)
-    target_path = Path(args.output_path)
     progress_path = Path(args.progress_db)
     fast_storage = Path(args.cache_path)
-    bypass_xy_align_errors = args.bypass_xy_align_errors
+
 
     bag_files = sorted(list(deployment_root_path.glob('**/*.bag')), key=lambda x: x.stat().st_size)
     progress_path.touch(exist_ok=True)
@@ -135,37 +187,30 @@ def run():
         recorded = yaml.safe_load(handle)
         if recorded:
             file_progress.update(recorded)
+    all_jobs: List[Job] = [Job(bag_file=bag_file,
+        data_root=deployment_root_path,
+        target_root=Path(args.output_path),
+        cache_path=fast_storage) for bag_file in bag_files]
 
-    bag_files = [f for f in bag_files
-        if '_'.join(f.relative_to(deployment_root_path).parts) not in file_progress]
-
-    output_dirs = ['_'.join(bag_file.relative_to(deployment_root_path).with_suffix('').parts)
-        for bag_file in bag_files]
-    label_dirs = ['_'.join(bag_file.relative_to(deployment_root_path).with_suffix('').parts) \
-        + '_label'for bag_file in bag_files]
-    tmp_paths = [fast_storage.joinpath('_'.join(bag_file.relative_to(deployment_root_path).parts))
-        for bag_file in bag_files]
-
-    for output_dir in tqdm(output_dirs):
-        target_path.joinpath(output_dir).mkdir(parents=True, exist_ok=True)
-    for label_dir in tqdm(label_dirs):
-        target_path.joinpath(label_dir).mkdir(parents=True, exist_ok=True)
+    jobs = [job for job in all_jobs if job.reference_name not in file_progress]
     fast_storage.mkdir(parents=True, exist_ok=True)
 
-    tmp_path_queue: "Queue[Path]" = Queue(maxsize=4)
+    job_queue: "Queue[Job]" = Queue(maxsize=4)
 
-    copy_thread = Thread(target=copy_thread_fn, args=(bag_files, tmp_paths, tmp_path_queue))
+    copy_thread = Thread(target=copy_thread_fn,
+        kwargs={
+            'jobs': jobs,
+            'job_queue': job_queue})
+    process_thread = Thread(target=process_thread_fn,
+        kwargs={
+            'job_queue': job_queue,
+            'progress_path': progress_path,
+            'file_progress': file_progress,
+            'num_jobs': len(jobs),
+            'bypass_xy_align_errors': args.bypass_xy_align_errors})
     copy_thread.start()
-    process_thread_args = {
-        'tmp_path_queue': tmp_path_queue,
-        'output_dirs': output_dirs,
-        'label_dirs': label_dirs,
-        'target_path': target_path,
-        'progress_path': progress_path,
-        'file_progress': file_progress,
-        'bypass_xy_align_errors': bypass_xy_align_errors}
-    process_thread = Thread(target=process_thread_fn, kwargs=process_thread_args)
     process_thread.start()
+
     process_thread.join()
     copy_thread.join()
 
