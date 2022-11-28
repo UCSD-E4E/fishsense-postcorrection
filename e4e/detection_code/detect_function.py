f3a360afdcfd348139eddb3de2c8e0ecd000c811
Nathan Hui <ntlhui@gmail.com>
2022-11-28 14:57:42 -0800

    pylint and optimizations

--- e4e/detection_code/detect_function.py
+++ e4e/detection_code/detect_function.py
@@ -1,42 +1,45 @@
-import os
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
 import cv2
 import numpy as np
 import tensorflow as tf
-from absl.flags import FLAGS
-from PIL import Image
-from tensorflow.compat.v1 import ConfigProto, InteractiveSession
 from tensorflow.python.saved_model import tag_constants
+from tqdm import tqdm
 
 import e4e.detection_code.core.utils as utils
-from e4e.detection_code.core.functions import *
-from e4e.detection_code.core.yolov4 import filter_boxes
+from e4e.detection_code.core.config import cfg
+from e4e.detection_code.core.functions import count_objects
+
 
 def detect(iou: float, score: float, images: List[Path]) -> List[Path]:
+    """Detects the fish images
+
+    Args:
+        iou (float): IoU threshold
+        score (float): Score threshold
+        images (List[Path]): List of images to process
+
+    Returns:
+        List[Path]: List of images that have fish
+    """
     #taking in input from given weights and input folders
     list_fishes: List[Path] = []
-    #saved_model_loaded = tf.saved_model.load("C:\\Users\\ragha\Desktop\\fishsense-postcorrection\\e4e\detection_code\\checkpoints\\yolov4-416\\", tags=[tag_constants.SERVING])
     saved_model_loaded = tf.saved_model.load("./yolov4-416", tags=[tag_constants.SERVING])
-    for input_file in images:
-        #The current weights are designed for 416 x 416 images, so preprocssing, recoloring and One-hot encoding
+    infer = saved_model_loaded.signatures['serving_default']
+    class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+    input_classes = list(class_names.values())
+    for input_file in tqdm(images):
+        # The current weights are designed for 416 x 416 images, so preprocssing, recoloring and
+        # One-hot encoding
         original_image = cv2.imread(input_file.as_posix())
-        original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+        rgb_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 
-        image_data = cv2.resize(original_image, (416, 416))
-        image_data = image_data / 255.
+        image_data = np.asarray([cv2.resize(rgb_img, (416, 416)) / 255.], dtype=np.float32)
 
-        #image data is saved 
-        images_data = []
-        images_data.append(image_data)
-        images_data = np.asarray(images_data).astype(np.float32)
-
-
-        infer = saved_model_loaded.signatures['serving_default']
-        batch_data = tf.constant(images_data)
-        pred_bbox = infer(batch_data)
-        for key, value in pred_bbox.items():
+        batch_data = tf.constant(image_data)
+        pred_bbox: Dict[str, tf.Tensor] = infer(batch_data)
+        for value in pred_bbox.values():
             boxes = value[:, :, 0:4]
             pred_conf = value[:, :, 4:]
 
@@ -55,13 +58,8 @@ def detect(iou: float, score: float, images: List[Path]) -> List[Path]:
         bboxes = utils.format_boxes(boxes.numpy()[0], original_h, original_w)
         pred_bbox = [bboxes, scores.numpy()[0], classes.numpy()[0], valid_detections.numpy()[0]]
 
-        class_names = utils.read_class_names(cfg.YOLO.CLASSES)
-        input_classes = list(class_names.values())
-
-        counted_things = (count_objects(pred_bbox, by_class=True, allowed_classes=input_classes))
-        if('Fish' in counted_things and counted_things['Fish'] != 0):
+        counted_things = count_objects(pred_bbox, by_class=True, allowed_classes=input_classes)
+        if 'Fish' in counted_things and counted_things['Fish'] != 0:
             list_fishes.append(input_file)
-            print("Finished evaluating: "+str(input_file)+ " which had "+str(counted_things['Fish'])+ " fishes")
-        else:
-            print("Finished evaluating: "+str(input_file)+ " which had 0 fishes")
+
     return list_fishes
