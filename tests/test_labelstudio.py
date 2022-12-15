c5942bb9a8ab2c86ce766882b1693ec4cd44c3c6
Nathan Hui <ntlhui@gmail.com>
2022-12-14 17:21:07 -0800

    Linting

--- tests/test_labelstudio.py
+++ tests/test_labelstudio.py
@@ -3,12 +3,17 @@
 from pathlib import Path
 from typing import Dict, Tuple
 
-from labelstudio_mock_data import create_test_export
+from labelstudio_mock_data import create_test_export # pylint: disable=unused-import
 
 from e4e.labelstudio import Point, Rectangle, extract_fish_annotations
 
 
 def test_extract_annotations(label_studio_export_json: Tuple[Path, Dict, Path]):
+    """Tests annotation extraction
+
+    Args:
+        label_studio_export_json (Tuple[Path, Dict, Path]): Sample export
+    """
     export_path, _, data_root = label_studio_export_json
     output = extract_fish_annotations(
         export_path=export_path,
